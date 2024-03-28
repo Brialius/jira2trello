d32657e72f04ed61b12ad3538acedf81f09dc52b
Denis Belyatsky <denis.bel@gmail.com>
2024-03-28 09:52:08 +0200

    Unify reports formatting

--- internal/app/report_template.go
+++ internal/app/report_template.go
@@ -16,7 +16,7 @@ const htmlTemplate = `<!DOCTYPE html>
 	<h1>Report {{ .Year }} week {{ .WeekNumber }}</h1>
 	<ul>
 {{- range $task := .Tasks }}
-	<li><a href={{ $task.Link }}>{{ $task.Key }}</a> {{ $task.Name }} - <strong>{{ $task.Status }}</strong></li>
+	<li><a href={{ $task.Link }}>{{ $task.Key }}</a> | {{ $task.Name }} - <strong>{{ $task.Status }}</strong></li>
 {{- end }}
 	</ul>
 </body>
